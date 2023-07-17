986a5ff0d834668449705c3cb2e40f665a355dcd
fioraaaa <fiora.berliana.tif420@polban.ac.id>
2023-07-17 11:33:26 +0700

    integrasi ganti password

--- backend/src/controllers/user.js
+++ backend/src/controllers/user.js
@@ -59,29 +59,41 @@ const login = async (req, res) => {
 
 const updateProfile = async (req, res) => {
     try {
-        let user = req.body
-        if(user.password){
-            let result = user.password.match(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{8,32}$/)
-            if(result){
-                const salt = bcrypt.genSaltSync(parseInt(process.env.SALT))
-                const password = bcrypt.hashSync(user.password, salt)
-                user.password = password
-            }
-            else
-                return res.status(400).json({message : "Password not valid"})
+      let user = req.body;
+  
+      if (user.password) {
+        let result = user.password.match(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{8,32}$/);
+        if (result) {
+          const salt = bcrypt.genSaltSync(parseInt(process.env.SALT));
+          const password = bcrypt.hashSync(user.password, salt);
+  
+          if (!user.oldPassword) {
+            return res.status(400).json({ message: "Old password is required" });
+          }
+  
+          // Memeriksa kecocokan password lama sebelum melakukan pembaruan
+          const isMatch = await bcrypt.compare(user.oldPassword, req.user.password);
+          if (!isMatch) {
+            return res.status(400).json({ message: "Old password is incorrect" });
+          }
+  
+          user.password = password; // Update password baru
+        } else {
+          return res.status(400).json({ message: "Password not valid" });
         }
-        else{
-            user.password = req.user.password
-        }
-        let keys = Object.keys(user)
-        await Users.validate(user, keys)
-        const result = await Users.findByIdAndUpdate(req.user.id, user)
-        res.status(200).json({message: "Profile updated successfully"})
+      } else {
+        user.password = req.user.password;
+      }
+  
+      let keys = Object.keys(user);
+      await Users.validate(user, keys);
+      const result = await Users.findByIdAndUpdate(req.user.id, user); // Update profil pengguna
+      res.status(200).json({ message: "Profile updated successfully" });
     } catch (error) {
-        res.status(400).json({message: error.message})
+      res.status(400).json({ message: error.message });
     }
-}
-
+  };
+  
 const deleteProfile = async (req, res) => {
     try {
         const userId = req.user.id;
@@ -243,7 +255,7 @@ const userController = {
     getAllUsers,
     getAllUsername,
     findUsersByUsername,
-    resetPassword
+    resetPassword 
 }
 
 module.exports = userController
--- frontend/src/components/Profile/ModalPassword.jsx
+++ frontend/src/components/Profile/ModalPassword.jsx
@@ -1,7 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
+import axios from '../../axiosConfig';
 
 const ModalPassword = () => {
   const [showModal, setShowModal] = useState(false);
+  const [oldPassword, setOldPassword] = useState('');
+  const [password, setNewPassword] = useState('');
+
+  const saveNewPassword = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      const response = await axios.patch(
+        "/api/user",
+        { oldPassword, password },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        }
+      );
+      console.log('Password updated successfully:', response.data);
+      setShowModal(false);
+    } catch (error) {
+      console.log('Error updating password:', error);
+    }
+  };
+
   return (
     <>
     <button className="text-white w-[30%] border-2 bg-[#2B5579] hover:bg-[#4ba3d3] rounded-full py-2 "
@@ -28,18 +51,31 @@ const ModalPassword = () => {
                     <label className="block text-black text-sm font-semibold mb-1">
                       Password Lama
                     </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black" />
+                    {/* <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black" /> */}
+                    <input
+                      className="shadow appearance-none border rounded w-full py-2 px-1 text-black"
+                      type="password"
+                      value={oldPassword}
+                      onChange={(e) => setOldPassword(e.target.value)}
+                    />
                     <label className="block text-black text-sm font-semibold mb-1 mt-3">
                       Password Baru
                     </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black" />
+                    {/* <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black" /> */}
+                    <input
+                      className="shadow appearance-none border rounded w-full py-2 px-1 text-black"
+                      type="password"
+                      value={password}
+                      onChange={(e) => setNewPassword(e.target.value)}
+                    />
                   </form>
                 </div>
                 <div className="flex items-center justify-end p-6 rounded-b">
                   <button
                     className="text-white bg-[#2B5579] hover:bg-[#4ba3d3 font-semibold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
                     type="button"
-                    onClick={() => setShowModal(false)}
+                    // onClick={() => setShowModal(false)}
+                    onClick={saveNewPassword}
                   >
                     simpan
                   </button>
